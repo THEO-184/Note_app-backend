a29395a17daae63fa0c01b77661c69e7af2b2653
Theophilus Boakye <93124888+TheophilusBoakye@users.noreply.github.com>
2022-10-14 15:53:20 +0000

    notes controller

---
+++ src/controllers/notes.controller.ts
@@ -0,0 +1,51 @@
+import { StatusCodes } from "http-status-codes";
+import { RequestHandler } from "express";
+import error from "../errors";
+import {
+	createNoteService,
+	deleteNoteService,
+	getNotesService,
+	updateNoteService,
+} from "../services/notes.service";
+import { NotesInput } from "../utils/types/notes.type";
+
+export const createNote: RequestHandler<{}, {}, NotesInput> = async (
+	req,
+	res
+) => {
+	const { body, title } = req.body;
+	if (!body || !title) {
+		throw new error.BadRequest("provide all fields");
+	}
+
+	const note = await createNoteService(req.body);
+
+	res.status(StatusCodes.CREATED).json({ note });
+};
+
+export const getNotes: RequestHandler = async (req, res) => {
+	const notes = await getNotesService();
+
+	res.status(StatusCodes.OK).json({ count: notes.length, notes });
+};
+
+export const updateNotes: RequestHandler<
+	{ id: string },
+	{},
+	NotesInput
+> = async (req, res) => {
+	const { body, title } = req.body;
+	if (!body || !title) {
+		throw new error.BadRequest("provide all fields");
+	}
+
+	const note = await updateNoteService({ _id: req.params.id }, req.body);
+
+	res.status(StatusCodes.OK).json({ note });
+};
+
+export const deleteNotes: RequestHandler<{ id: string }> = async (req, res) => {
+	await deleteNoteService({ _id: req.params.id });
+
+	res.status(StatusCodes.OK).json({ msg: "deleted successfully" });
+};
