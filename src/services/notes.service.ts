c2899ae6f691eaa576aade5bb8afe4057af2f35e
Theophilus Boakye <93124888+TheophilusBoakye@users.noreply.github.com>
2022-10-14 15:54:01 +0000

    notes services

---
+++ src/services/notes.service.ts
@@ -0,0 +1,42 @@
+import { RequestHandler } from "express";
+import { FilterQuery, UpdateQuery } from "mongoose";
+import error from "../errors";
+import Notes from "../model/notes.model";
+import { NotesDocument, NotesInput } from "../utils/types/notes.type";
+
+export const createNoteService = async (input: NotesInput) => {
+	const note = await Notes.create(input);
+
+	return note;
+};
+
+export const getNotesService = async () => {
+	const notes = await Notes.find({}).sort("-updatedAt -createdAt");
+	return notes;
+};
+
+export const updateNoteService = async (
+	filter: FilterQuery<NotesDocument>,
+	update: UpdateQuery<NotesDocument>
+) => {
+	const note = await Notes.findOneAndUpdate(filter, update, {
+		new: true,
+		runValidators: true,
+	});
+
+	if (!note) {
+		throw new error.NotFound("no note found with the given id");
+	}
+
+	return note;
+};
+
+export const deleteNoteService = async (filter: FilterQuery<NotesDocument>) => {
+	const note = await Notes.findOneAndDelete(filter, {
+		new: true,
+		runValidators: true,
+	});
+	if (!note) {
+		throw new error.NotFound("no note found with the given id");
+	}
+};
