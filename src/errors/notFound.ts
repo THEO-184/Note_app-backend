0df34657ce7dc00e7ac209f03382ff1ce75437f5
Theophilus Boakye <93124888+TheophilusBoakye@users.noreply.github.com>
2022-10-14 15:07:55 +0000

    error handlers

---
+++ src/errors/notFound.ts
@@ -0,0 +1,12 @@
+import { StatusCodes } from "http-status-codes";
+import CustomError from "./default-error";
+
+class NotFound extends CustomError {
+	statusCode: number;
+	constructor(message: string) {
+		super(message);
+		this.statusCode = StatusCodes.NOT_FOUND;
+	}
+}
+
+export default NotFound;
