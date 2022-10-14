e1212a5833770d54e1a70f67a026d729487ebd45
Theophilus Boakye <93124888+TheophilusBoakye@users.noreply.github.com>
2022-10-14 15:53:39 +0000

    set title unique

--- src/model/notes.model.ts
+++ src/model/notes.model.ts
@@ -5,6 +5,7 @@ const NotesSchema = new Schema<NotesDocument>(
 	{
 		title: {
 			type: String,
+			unique: true,
 			required: [true, "provide note title"],
 			minlength: [3, "title must be atleast 4 chracters"],
 			maxlength: [35, "title must be atmost 35 chracters"],
