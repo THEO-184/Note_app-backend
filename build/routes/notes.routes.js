576ca5cc54d7562ffc5d03786869189f73bcbce1
Theophilus Boakye <93124888+TheophilusBoakye@users.noreply.github.com>
2022-10-17 15:39:50 +0000

    empty build folder

--- build/routes/notes.routes.js
+++
@@ -1,8 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-const express_1 = require("express");
-const notes_controller_1 = require("../controllers/notes.controller");
-const router = (0, express_1.Router)();
-router.route("/").post(notes_controller_1.createNote).get(notes_controller_1.getNotes);
-router.route("/:id").put(notes_controller_1.updateNotes).delete(notes_controller_1.deleteNotes);
-exports.default = router;
