356c303b7f75ae374f6fc528ad3ea6ee3d3b6850
Theophilus Boakye <93124888+TheophilusBoakye@users.noreply.github.com>
2022-10-17 15:49:57 +0000

    add build

---
+++ build/app.js
@@ -0,0 +1,46 @@
+"use strict";
+var __awaiter = (this && this.__awaiter) || function (thisArg, _arguments, P, generator) {
+    function adopt(value) { return value instanceof P ? value : new P(function (resolve) { resolve(value); }); }
+    return new (P || (P = Promise))(function (resolve, reject) {
+        function fulfilled(value) { try { step(generator.next(value)); } catch (e) { reject(e); } }
+        function rejected(value) { try { step(generator["throw"](value)); } catch (e) { reject(e); } }
+        function step(result) { result.done ? resolve(result.value) : adopt(result.value).then(fulfilled, rejected); }
+        step((generator = generator.apply(thisArg, _arguments || [])).next());
+    });
+};
+var __importDefault = (this && this.__importDefault) || function (mod) {
+    return (mod && mod.__esModule) ? mod : { "default": mod };
+};
+Object.defineProperty(exports, "__esModule", { value: true });
+const dotenv_1 = __importDefault(require("dotenv"));
+dotenv_1.default.config();
+require("express-async-errors");
+const body_parser_1 = __importDefault(require("body-parser"));
+const express_1 = __importDefault(require("express"));
+const morgan_1 = __importDefault(require("morgan"));
+const cors_1 = __importDefault(require("cors"));
+// local imports
+const notes_routes_1 = __importDefault(require("./routes/notes.routes"));
+const db_1 = __importDefault(require("./db/db"));
+const express_middleware_1 = __importDefault(require("./middleware/express-middleware"));
+const app = (0, express_1.default)();
+app.use((0, cors_1.default)());
+app.use((0, morgan_1.default)("tiny"));
+app.use(body_parser_1.default.json());
+app.use(body_parser_1.default.urlencoded({ extended: false }));
+// routes
+app.use("/api/v1/notes", notes_routes_1.default);
+app.use(express_middleware_1.default);
+const PORT = 5000;
+const start = () => __awaiter(void 0, void 0, void 0, function* () {
+    try {
+        yield (0, db_1.default)(process.env.MONGO_URI);
+        app.listen(PORT, () => {
+            console.log(`server running on ${PORT}`);
+        });
+    }
+    catch (error) {
+        console.log("error", error);
+    }
+});
+start();
